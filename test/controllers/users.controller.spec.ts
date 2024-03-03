858bac0d4710ed062d790d10dfe7c1001785077c
Pavel <pavelromanov1226@gmail.com>
2024-03-04 00:20:51 +0300

    feat: add tests for users

--- test/controllers/users.controller.spec.ts
+++ test/controllers/users.controller.spec.ts
@@ -2,12 +2,24 @@ import { INestApplication } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { ConfigModule } from '@nestjs/config';
-import { SequelizeConfigService } from '../../src/config/sequelizeConfig.service';
-import { databaseConfig } from '../../src/config/configuration';
-import { UsersModule } from '../../src/users/users.module';
-import { User } from '../../src/users/users.model';
+import { SequelizeConfigService } from 'src/config/sequelizeConfig.service';
+import { databaseConfig } from 'src/config/configuration';
+import { UsersModule } from 'src/users/users.module';
+import { User } from 'src/users/users.model';
 import * as request from 'supertest';
 import * as bcrypt from 'bcrypt';
+import * as session from 'express-session';
+import * as passport from 'passport';
+import { AuthModule } from 'src/auth/auth.module';
+
+const newUser = {
+  name: 'TestName',
+  surname: 'TestSurname',
+  hireDate: new Date('2023-09-11T21:53:58.386Z'),
+  email: 'test8@email.com',
+  password: 'password123',
+  role: 'user',
+};
 
 describe('Users Controller', () => {
   let app: INestApplication;
@@ -23,35 +35,122 @@ describe('Users Controller', () => {
           load: [databaseConfig],
         }),
         UsersModule,
+        AuthModule,
       ],
     }).compile();
 
     app = testModule.createNestApplication();
+    app.use(
+      session({
+        secret: 'keyword',
+        resave: false,
+        saveUninitialized: false,
+      }),
+    );
+    app.use(passport.initialize());
+    app.use(passport.session());
+
     await app.init();
   });
 
   afterEach(async () => {
-    await User.destroy({ where: { email: 'example@email.com' } });
+    await User.destroy({ where: { email: newUser.email } });
   });
 
-  it('should create user', async () => {
-    const newUser = {
-      name: 'TestName',
-      surname: 'TestSurname',
-      hireDate: new Date('2023-09-11T21:53:58.386Z'),
-      email: 'example@email.com',
-      password: 'password123',
-    };
-
+  it('Test Rest API Create User', async () => {
     const response = await request(app.getHttpServer())
       .post('/users/signup')
       .send(newUser);
 
-    const passwordIsValid = await bcrypt.compare(newUser.password, response.body.password);
+    const passwordIsValid = await bcrypt.compare(
+      newUser.password,
+      response.body.password,
+    );
 
     expect(response.body.name).toBe(newUser.name);
     expect(passwordIsValid).toBe(true);
     expect(response.body.email).toBe(newUser.email);
     expect(response.body.surname).toBe(newUser.surname);
+  });
+
+  it('Test Rest API Update User', async () => {
+    await request(app.getHttpServer()).post('/users/signup').send(newUser);
+
+    const login = await request(app.getHttpServer())
+      .post('/users/login')
+      .send({ email: newUser.email, password: newUser.password });
+
+    const response = await request(app.getHttpServer())
+      .put(`/users/update/${login.body.user.userId}`)
+      .send({ name: 'updated' })
+      .set('Cookie', login.headers['set-cookie']);
+
+    const passwordIsValid = await bcrypt.compare(
+      newUser.password,
+      response.body.password,
+    );
+
+    expect(response.body.name).toBe('updated');
+    expect(passwordIsValid).toBe(true);
+    expect(response.body.email).toBe(newUser.email);
+    expect(response.body.surname).toBe(newUser.surname);
+  });
+
+  it('Test Rest API Delete User', async () => {
+    await request(app.getHttpServer()).post('/users/signup').send(newUser);
+
+    const login = await request(app.getHttpServer())
+      .post('/users/login')
+      .send({ email: newUser.email, password: newUser.password });
+
+    const response = await request(app.getHttpServer())
+      .delete(`/users/delete/${login.body.user.userId}`)
+      .set('Cookie', login.headers['set-cookie']);
+
+    expect(response.body.message).toEqual('Пользователь удален');
+  });
+
+  it('Test Rest API Get User By ID 1', async () => {
+    await request(app.getHttpServer()).post('/users/signup').send(newUser);
+
+    const login = await request(app.getHttpServer())
+      .post('/users/login')
+      .send({ email: newUser.email, password: newUser.password });
+
+    const response = await request(app.getHttpServer())
+      .get(`/users/find/${login.body.user.userId}`)
+      .set('Cookie', login.headers['set-cookie']);
+
+    expect(response.body.id).toEqual(login.body.user.userId);
+  });
+
+  it('Test Rest API Get Users', async () => {
+    await request(app.getHttpServer()).post('/users/signup').send(newUser);
+
+    const login = await request(app.getHttpServer())
+      .post('/users/login')
+      .send({ email: newUser.email, password: newUser.password });
+
+    const response = await request(app.getHttpServer())
+      .get('/users?limit=20&offset=0')
+      .set('Cookie', login.headers['set-cookie']);
+
+    response.body.rows.forEach((item) => {
+      expect(item).toEqual(
+        expect.objectContaining({
+          id: expect.any(Number),
+          name: expect.any(String),
+          surname: expect.any(String),
+          hireDate: expect.any(String),
+          email: expect.any(String),
+          password: expect.any(String),
+          role: expect.any(String),
+          createdAt: expect.any(String),
+          updatedAt: expect.any(String),
+        }),
+      );
+    });
+
+    expect(response.body.count).toEqual(expect.any(Number));
   });
 });
--- test/providers/users.service.spec.ts
+++ test/providers/users.service.spec.ts
@@ -9,6 +9,15 @@ import { databaseConfig } from 'src/config/configuration';
 import { UsersModule } from 'src/users/users.module';
 import { User } from 'src/users/users.model';
 
+const newUser = {
+  name: 'TestName',
+  surname: 'TestSurname',
+  hireDate: new Date('2023-09-11T21:53:58.386Z'),
+  email: 'test8@email.com',
+  password: 'password123',
+  role: 'user',
+};
+
 describe('Users Service', () => {
   let app: INestApplication;
   let usersService: UsersService;
@@ -33,19 +42,10 @@ describe('Users Service', () => {
   });
 
   afterEach(async () => {
-    await User.destroy({ where: { email: 'example@email.com' } });
+    await User.destroy({ where: { email: newUser.email } });
   });
 
-  it('should create user', async () => {
-    const newUser = {
-      name: 'TestName',
-      surname: 'TestSurname',
-      hireDate: new Date('2023-09-11T21:53:58.386Z'),
-      email: 'example@email.com',
-      password: 'password123',
-      role: 'user',
-    };
-
+  it('Test Create User', async () => {
     const user = (await usersService.create(newUser)) as User;
 
     const passwordIsValid = await bcrypt.compare(
@@ -59,5 +59,88 @@ describe('Users Service', () => {
     expect(user.surname).toBe(newUser.surname);
     expect(user.hireDate).toBe(newUser.hireDate);
     expect(user.role).toBe(newUser.role);
+  });
+
+  it('Test Update User', async () => {
+    const user = (await usersService.create(newUser)) as User;
+
+    const updatedUser = await usersService.update(user.id, {
+      id: user.id,
+      name: 'updated',
+    });
+
+    const passwordIsValid = await bcrypt.compare(
+      newUser.password,
+      user.password,
+    );
+
+    expect(passwordIsValid).toBe(true);
+
+    expect(updatedUser).toEqual(
+      expect.objectContaining({
+        name: 'updated',
+        surname: newUser.surname,
+        hireDate: expect.any(Date),
+        email: newUser.email,
+        password: expect.any(String),
+        role: newUser.role,
+        createdAt: expect.any(Date),
+        updatedAt: expect.any(Date),
+      }),
+    );
+  });
+
+  it('Test Delete User', async () => {
+    const user = (await usersService.create(newUser)) as User;
+
+    const result = await usersService.delete(user.id);
+
+    expect(result.message).toEqual('Пользователь удален');
+  });
+
+  it('Test Find By ID 1 (findOne Method)', async () => {
+    const createdUser = (await usersService.create(newUser)) as User;
+    const user = await usersService.findOne({
+      where: { id: createdUser.id },
+    });
+
+    expect(user).toEqual(
+      expect.objectContaining({
+        id: createdUser.id,
+        name: expect.any(String),
+        surname: expect.any(String),
+        hireDate: expect.any(Date),
+        email: expect.any(String),
+        password: expect.any(String),
+        role: expect.any(String),
+        createdAt: expect.any(Date),
+        updatedAt: expect.any(Date),
+      }),
+    );
+  });
+
+  it('Test Paginate Method', async () => {
+    const users = await usersService.paginate({
+      limit: '20',
+      offset: '1',
+    });
+
+    users.rows.forEach((item) => {
+      expect(item.dataValues).toEqual(
+        expect.objectContaining({
+          id: expect.any(Number),
+          name: expect.any(String),
+          surname: expect.any(String),
+          hireDate: expect.any(Date),
+          email: expect.any(String),
+          password: expect.any(String),
+          role: expect.any(String),
+          createdAt: expect.any(Date),
+          updatedAt: expect.any(Date),
+        }),
+      );
+    });
+
+    expect(users.count).toEqual(expect.any(Number));
   });
 });
